90a2d7cb94017b9b0a27ea6506a8b6ea26e0b168
Jeongho Park <d0lbangi13@naver.com>
2023-07-23 01:39:39 +0900

    FEAT: Adding Items to an Array with  CreateUser.js

---
+++ src/UserList.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const User = ({user, onRemove}) => {
+  return (
+    <div>
+      <b>{user.username}</b> <span>{user.email}</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  )
+}
+
+const UserList = ({users, onRemove}) => {
+  return(
+    <>
+      {users.map(user => (
+        <User user={user} key={user.id} onRemove={onRemove} />
+      ))}
+    </>
+  );
+}
+
+export default UserList
