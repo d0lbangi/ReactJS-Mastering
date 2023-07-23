798f5f9549eadc36b2c9629b1954fea2078592b0
Jeongho Park <d0lbangi13@naver.com>
2023-07-23 16:23:41 +0900

    feat: Modifying Array Items

--- src/UserList.js
+++ src/UserList.js
@@ -1,22 +1,37 @@
-import React from 'react'
+import React from 'react';
 
-const User = ({user, onRemove}) => {
+function User({ user, onRemove, onToggle }) {
   return (
     <div>
-      <b>{user.username}</b> <span>{user.email}</span>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
-  )
-}
-
-const UserList = ({users, onRemove}) => {
-  return(
-    <>
-      {users.map(user => (
-        <User user={user} key={user.id} onRemove={onRemove} />
-      ))}
-    </>
   );
 }
 
-export default UserList
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default UserList;
