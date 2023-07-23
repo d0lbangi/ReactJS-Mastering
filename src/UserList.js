eb0eddaf94fb04c468ce18265f40ae26b4fed388
Jeongho Park <d0lbangi13@naver.com>
2023-07-23 18:58:13 +0900

    FEAT: Context API 를 사용한 전역 값 관리

--- src/UserList.js
+++ src/UserList.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserDispatch } from './App';
 
-const CreateUser = ({ username, email, onChange, onCreate }) => {
+const User = React.memo(function User({ user }) {
+  const dispatch = useContext(UserDispatch);
+
   return (
     <div>
-      <input
-        name="username"
-        placeholder="계정명"
-        onChange={onChange}
-        value={username}
-      />
-      <input
-        name="email"
-        placeholder="이메일"
-        onChange={onChange}
-        value={email}
-      />
-      <button onClick={onCreate}>등록</button>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => {
+          dispatch({ type: 'TOGGLE_USER', id: user.id });
+        }}
+      >
+        {user.username}
+      </b>
+      &nbsp;
+      <span>({user.email})</span>
+      <button
+        onClick={() => {
+          dispatch({ type: 'REMOVE_USER', id: user.id });
+        }}
+      >
+        삭제
+      </button>
     </div>
   );
-};
+});
 
-export default React.memo(CreateUser);
+function UserList({ users }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} key={user.id} />
+      ))}
+    </div>
+  );
+}
+
+export default React.memo(UserList);
