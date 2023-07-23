00a1ecc43c2fcfda20a99f0a37f0e720bff27278
Jeongho Park <d0lbangi13@naver.com>
2023-07-23 17:26:22 +0900

    FEAT: useCallback 을 사용하여 함수 재사용하기

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useMemo } from 'react';
+import React, { useRef, useState, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
 
@@ -13,13 +13,16 @@ const App = () => {
     email: ''
   });
   const { username, email } = inputs;
-  const onChange = e => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value
-    });
-  };
+  const onChange = useCallback(
+    e => {
+      const { name, value } = e.target;
+      setInputs({
+        ...inputs,
+        [name]: value
+      });
+    },
+    [inputs]
+  );
   const [users, setUsers] = useState([
     {
       id: 1,
@@ -42,7 +45,7 @@ const App = () => {
   ]);
 
   const nextId = useRef(4);
-  const onCreate = () => {
+  const onCreate = useCallback(() => {
     const user = {
       id: nextId.current,
       username,
@@ -55,21 +58,27 @@ const App = () => {
       email: ''
     });
     nextId.current += 1;
-  };
+  }, [users, username, email]);
 
-	const onRemove = id => {
+	const onRemove = useCallback(
+    id => {
 		// user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열 맏늚
 		// user.id 가 id인 것을 제거함
-		setUsers(users.filter(user => user.id !== id));
-	};
+		  setUsers(users.filter(user => user.id !== id));
+	  },
+  [users]
+  );
 
-  const onToggle = id => {
-    setUsers(
-      users.map(user =>
-      user.id === id ? {...user, active: !user.active} : user
-      )
-    );
-  };
+  const onToggle = useCallback(
+    id => {
+      setUsers(
+        users.map(user =>
+        user.id === id ? {...user, active: !user.active} : user
+        )
+      );
+    },
+  [users]
+  );
   // 초기 코드
   // const count = countActiveUsers(users);
 
