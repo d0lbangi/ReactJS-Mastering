390459366c2b518ea9eb5a652d26191cd4d2e8fb
Jeongho Park <d0lbangi13@naver.com>
2023-07-23 19:44:29 +0900

    FEAT: Immer 를 사용한 더 쉬운 불변성 관리

--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from "react-dom";
 import './index.css';
 import App from "./App";
 // import * as serviceWorker from './serviceWorker';
-import Counter from './Counter';
 import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<Counter />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
